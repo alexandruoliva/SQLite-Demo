756193bc1c8e6241e3fef94efc2320db7cd81291
unknown <olivaalexandru@yahoo.com>
2020-12-01 16:42:49 +0200

    Added the SQL statement to create tabble

--- app/src/main/java/com/example/sqlitedemo/DataBaseHelper.java
+++ app/src/main/java/com/example/sqlitedemo/DataBaseHelper.java
@@ -11,6 +11,12 @@ import androidx.annotation.Nullable;
 public class DataBaseHelper extends SQLiteOpenHelper {
 
 
+    public static final String CUSTOMER_TABLE = "CUSTOMER_TABLE";
+    public static final String COLUMN_CUSTOMER_NAME = "CUSTOMER_NAME";
+    public static final String COLUMN_CUSTOMER_AGE = "CUSTOMER_AGE";
+    public static final String COLUMN_ACTIVE_CUSTOMER = "ACTIVE_CUSTOMER";
+    public static final String COLUMN_ID = "ID";
+
     public DataBaseHelper(@Nullable Context context) {
         super(context, "customer.db", null, 1);
     }
@@ -18,7 +24,9 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     //this is called the first time a database is accessed. There should be code in here to create a new table
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String createTableStatement = "CREATE TABLE " + CUSTOMER_TABLE + " (" + COLUMN_ID + "  INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_CUSTOMER_NAME + " TEXT, " + COLUMN_CUSTOMER_AGE + " INT, " + COLUMN_ACTIVE_CUSTOMER + " BOOL)";
 
+        db.execSQL(createTableStatement);
     }
 
     //this is called if the database version number changes. It prevents previous users apps from breaking when you change the database design.
